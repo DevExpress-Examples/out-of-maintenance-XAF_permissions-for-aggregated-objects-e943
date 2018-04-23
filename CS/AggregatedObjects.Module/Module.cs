ada97e27061029f7563d3dfb51f2defcf581eb89
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AggregatedObjects.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace AggregatedObjects.Module {
+    public sealed partial class AggregatedObjectsModule : ModuleBase {
+        public AggregatedObjectsModule() {
+            InitializeComponent();
+        }
+    }
+}
